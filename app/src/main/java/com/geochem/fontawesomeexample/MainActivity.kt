9923c41299b4562eba2c36d339f6b0a68775afb0
Abhishek <krishnatrye00@gmail.com>
2021-02-24 22:04:39 +0530

    second

--- app/src/main/java/com/geochem/fontawesomeexample/MainActivity.kt
+++ app/src/main/java/com/geochem/fontawesomeexample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.geochem.fontawesomeexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 //first
+//second
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
